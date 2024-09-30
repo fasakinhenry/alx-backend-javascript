bc48b57371ac616b17922ac85122d720a982288d
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-01 00:53:49 +0100

    perf: decrease line of code using function parameters

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -3,12 +3,5 @@ export default function getSumOfHoods(
   expansion1989 = 89,
   expansion2019 = 19
 ) {
-  if (expansion1989 === undefined) {
-    expansion1989 = 89;
-  }
-
-  if (expansion2019 === undefined) {
-    expansion2019 = 19;
-  }
   return initialNumber + expansion1989 + expansion2019;
 }
